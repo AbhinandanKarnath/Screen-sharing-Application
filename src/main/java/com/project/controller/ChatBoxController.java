3c409f50bcb8431bd0862da4b7995cee0560b0c8
noobmaster52 <abhinandankaranth52@gmail.com>
2023-08-05 20:41:19 +0530

    Made the message box background color black and added scroll bar to it

--- src/main/java/com/project/controller/ChatBoxController.java
+++ src/main/java/com/project/controller/ChatBoxController.java
@@ -1,13 +1,17 @@
 package com.project.controller;
 
 import javafx.application.Platform;
+import javafx.geometry.Insets;
 import javafx.fxml.FXML;
 import javafx.concurrent.Task;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+
+import java.awt.*;
 
 import static com.project.model.Message.*;
 
@@ -44,7 +48,10 @@ public class ChatBoxController
 
                             Platform.runLater(() -> {
                                 Label label = new Label(message);
-                                label.setFont(new Font("Arial",16));
+                                label.setFont(new Font("Arial",15));
+
+                                label.setStyle("-fx-background-color: rgb(20,20,20); -fx-background-radius: 15px; -fx-text-fill: #ffffff; -fx-padding: 5px");
+                                label.setWrapText(true);
                                 vBox.getChildren().add(label);
                             });
                         }
@@ -55,6 +62,8 @@ public class ChatBoxController
                 sendMessageToEveryone(" is in the Chat");
             } else {
                 receiveMessagesTask.cancel();
+
+                sendMessageToEveryone(" logged off");
                 closeChatConnection();
             }
         }
